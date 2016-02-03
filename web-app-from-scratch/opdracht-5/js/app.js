1592501a766c293c4c6994eb4abd4adaa8f89e6a
Matth96 <Matthias.Dolstra@hva.nl>
2016-02-03 15:00:34 +0100

    setup web app from stretch app.

--- web-app-from-scratch/opdracht-5/js/app.js
+++ web-app-from-scratch/opdracht-5/js/app.js
@@ -1,4 +1,39 @@
-"use strict"
-//app.init()
-//routes.init()
-//sections.toggle(route)
+//Maakt de variable app aanmaken als globaal object. En als er al een varianle app bestaat voegd hij de app var toe. Dit voorkomt conflicten met andere libary's
+var app = app || {};
+
+// use IIFE to avoid global vars
+(function () {
+    "use strict"
+
+    app.start = {
+        init: function () {
+            app.router.init();
+            app.sections.toggle();
+            console.log('app.init uitgevoerd')
+        }
+    };
+    app.router = {
+        init: function () {
+            console.log('app.router.init uitgevoerd')
+            this.OnHashChange()
+        },
+        OnHashChange: function (event) {
+            onhashchane(function () {
+                alert("verandert");
+            })
+
+
+        },
+        ChangeHash: function () {
+            document.location.hash = "myBookmark";
+        }
+
+    }
+    app.sections = {
+            toggle: function () {
+                console.log('app.sections.toggle uitgevoerd')
+            }
+        }
+        //voer de app uit
+    app.start.init()
+}())
