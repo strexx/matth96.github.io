4939ed078c7dd617f897b9dbcaa8a835d5a88844
Matth96 <Matthias.Dolstra@hva.nl>
2016-02-11 19:05:33 +0100

    bug fixes
    fix bug with search,
    add promises,
    rename vars

--- web-app-from-scratch/opdracht-6/js/app.js
+++ web-app-from-scratch/opdracht-6/js/app.js
@@ -10,8 +10,7 @@ var app = app || {};
         init: function () { //Start object in this object are all start functions
             app.support.init();
             app.routes.init();
-            localStorage.setItem('savedCitys', "[]");
-            console.log('app started');
+            app.localStorage.init();
         }
     };
 
@@ -28,18 +27,59 @@ var app = app || {};
                 newTemp = (Math.round(rawTemp * 100) / 100).toFixed(1);
 
             return newTemp
+        },
+        data: function (url) {
+
+            // return a Promise object
+            return new Promise((resolve, reject) => {
+                var request = new XMLHttpRequest();
+
+                request.open('GET', url);
+                request.onload = function () {
+                    //ony if request is done
+                    if (request.status == 200) {
+
+                        // send resolve
+                        resolve(request.responseText);
+                    } else {
+                        // reject the promise
+                        reject(new Error('request failed!'));
+                    }
+                };
+                request.send();
+            });
+        },
+    };
+
+    app.render = {
+        template: function (target, template, data) {
+            app.get.one(target).innerHTML = Mustache.render(template, {
+                data: data
+            });
         }
     };
+
     app.localStorage = {
+        init: function () {
+            if (localStorage.getItem('savedCitys') === null || localStorage.getItem('savedCitys') === undefined) {
+                localStorage.setItem('savedCitys', "[]");
+            }
+        },
         get: function () {
             var savedCitys = localStorage.getItem('savedCitys');
             return JSON.parse(savedCitys)
         },
         add: function (data) {
-            var savedCitys = this.get();
-            savedCitys.push(data);
-            var stringifiedData = JSON.stringify(savedCitys);
-            localStorage.setItem('savedCitys', stringifiedData);
+            var savedCitys = this.get(),
+                contains = _.contains(savedCitys, data);
+
+            if (contains === false) {
+                savedCitys.push(data);
+                var stringifiedData = JSON.stringify(savedCitys);
+                localStorage.setItem('savedCitys', stringifiedData);
+            } else {
+                console.log("bestaat al")
+            }
         }
     };
 
@@ -54,8 +94,10 @@ var app = app || {};
                     '/city/:city': app.page.city
                 },
                 router = Router(routes);
-
             router.init();
+            if (location.hash === undefined || location.hash === "") {
+                window.location = "#/home"
+            }
         }
     };
 
@@ -76,134 +118,146 @@ var app = app || {};
         }
     };
 
-
-    //Source https://stackoverflow.com/questions/247483/http-get-request-in-javascript
-    var HttpClient = function () { // this is a constructor
-        this.get = function (aUrl, aCallback) {
-            var anHttpRequest = new XMLHttpRequest();
-
-            anHttpRequest.onreadystatechange = function () {
-                if (anHttpRequest.readyState == 4 && anHttpRequest.status == 200)
-                    aCallback(anHttpRequest.responseText);
-            }
-            anHttpRequest.open("GET", aUrl, true);
-            anHttpRequest.send(null);
-        };
-    };
-
     //define all pages in the app.
     app.page = {
         init: function () {
             location.replace('#/home');
         },
         home: function () {
-            var GetTemplate = new HttpClient();
-
-            GetTemplate.get('./temp/home.mst', function (response) {
+            app.get.data('./temp/home.mst').then(response => {
                 app.get.one('#target').innerHTML = Mustache.render(response);
+            }).catch(e => {
+                console.error(e);
             });
+
         },
         search: function () {
-            var Getemplate = new HttpClient();
-            Getemplate.get('./temp/search.mst', function (response) {
-                var responseTemplate = response;
-                app.get.one('#target').innerHTML = Mustache.render(responseTemplate, {
-                    searchresult: []
+            var searchQuery = "Ermelo",
+                searchResultsTemplate = "",
+                searchField = "",
+                searchFuntion = function (searchField) {
+                    searchField.addEventListener('input', function () {
+                        app.get.data(app.data.SearchUrl(searchField.value))
+                            .then(response => {
+                                var data = JSON.parse(response).list
 
-                });
-                var searchCity = new HttpClient(),
-                    searchQuery = app.get.one('#search');
-
-                searchQuery.addEventListener('input', function () {
-                    if (searchQuery.value.length > 2) {
-                        searchCity.get(app.data.SearchUrl(searchQuery.value), function (response) {
-                            var search = JSON.parse(response).list;
-
-                            app.get.one('#target').innerHTML = Mustache.render(responseTemplate, {
-                                searchresult: search
-                            });
-                            app.get.one(".searchlist").addEventListener("click", function (e) {
-                                if (e.target && e.target.nodeName == "LI") {
-                                    app.localStorage.add(e.target.innerHTML)
+                                if (app.get.one(".in-nl").checked === true) {
+                                    var rawData = _.filter(data, function (searchData, predicate) {
+                                        return searchData.sys.country === "NL";
+                                    });
+                                } else {
+                                    var rawData = _.filter(data, function (searchData) {
+                                        return searchData;
+                                    });
                                 }
+                                app.render.template("#searchresults", searchResultsTemplate, rawData);
+                                app.get.one(".searchlist").addEventListener("click", function (e) {
+                                    if (e.target && e.target.nodeName == "LI") {
+                                        app.localStorage.add(e.target.innerHTML)
+                                        window.location = "#/citys"
+                                    }
+                                });
                             });
+                    });
+                };
 
-                        });
-                    }
-                });
-            });
+            Promise.all([app.get.data('./temp/search.mst'), app.get.data('./temp/searchresults.mst')])
+                .then(response => {
+                    searchResultsTemplate = response[1]
+                    app.render.template("#target", response[0]);
+                })
+                .then(response => {
+                    var searchField = app.get.one('#search');
+                    searchFuntion(searchField);
+                })
+                .catch(err => {
+                    console.log(err);
+                })
         },
         citys: function () {
             var savedCitysData = [],
-                savedCitys = app.localStorage.get(),
-                weatherCity = new HttpClient();
+                savedCitys = app.localStorage.get()
 
-            savedCitys.forEach(function (element, index, array) {
-                weatherCity.get(app.data.WeatherUrl(savedCitys[index]), function (response) {
-                    var data = JSON.parse(response);
-
-                    savedCitysData.push({
-                        cityName: data.name,
-                        description: data.weather[0].description,
-                        temp: data.main.temp,
-                        wind: data.wind.speed
-                    })
-                    if (savedCitysData.length === savedCitys.length) {
-                        var Getemplate = new HttpClient();
-
-                        Getemplate.get('./temp/citys.mst', function (response) {
-                            app.get.one('#target').innerHTML = Mustache.render(response, {
-                                citys: savedCitysData
-                            });
+            app.get.data('./temp/citys.mst').then(response => {
+                var data = response;
+                return data;
+            }).then(response => {
+                savedCitys.forEach(function (element, index, array) {
+                    app.get.data(app.data.WeatherUrl(savedCitys[index])).then(response => {
+                        var data = JSON.parse(response);
+                        savedCitysData.push({
+                            cityName: data.name,
+                            cityNameUrl: data.name.replace(/ /g, '-'),
+                            description: data.weather[0].description,
+                            temp: data.main.temp,
                         });
-                    }
+                        if (savedCitysData.length === savedCitys.length) {
+                            app.get.data('./temp/citys.mst').then(response => {
+                                app.render.template("#target", response, savedCitysData)
+                            }).catch(e => {
+                                // catching all failures!
+                                console.error(e);
+                            });
+                        }
+                    }).catch(e => {
+                        console.error(e);
+                    });
                 });
+            }).catch(e => {
+                console.error(e);
             });
         },
         city: function (cityParam) {
-            var weatherCity = new HttpClient();
+            app.get.data(app.data.WeatherUrl(cityParam))
+                .then(response => {
+                    var data = JSON.parse(response);
+                    return data;
+                }).then(response => {
+                    var cityData = response;
+                    app.get.data('./temp/city.mst')
+                        .then(response => {
+                            app.render.template("#target", response, {
+                                cityName: cityData.name,
+                                description: cityData.weather[0].description,
+                                minTemp: cityData.main.temp_min,
+                                maxTemp: cityData.main.temp_max,
+                                temp: cityData.main.temp,
+                                clouds: cityData.clouds.all,
+                                wind: cityData.wind.speed
+                            });
+                        }).catch(e => {
+                            console.error(e);
+                        });
+                }).then(response => {
 
-            weatherCity.get(app.data.WeatherUrl(cityParam), function (response) {
-                var data = JSON.parse(response),
-                    Getemplate = new HttpClient();
-
-                Getemplate.get('./temp/city.mst', function (response) {
-                    app.get.one('#target').innerHTML = Mustache.render(response, {
-                        cityName: data.name,
-                        description: data.weather[0].description,
-                        minTemp: data.main.temp_min,
-                        maxTemp: data.main.temp_max,
-                        temp: data.main.temp,
-                        clouds: data.clouds.all,
-                        wind: data.wind.speed
-
-                    });
+                }).catch(e => {
+                    console.error(e);
                 });
-            });
         }
     };
 
     //Check if all functions are supported, if not, show an error message at top of the app.
     app.support = {
         init: function () {
-            this.onhashchange()
-            this.online()
+            this.onhashchange();
+            this.online();
         },
         onhashchange: function () {
             //Check if onhashchange is in the window object if not show a error
             if ("onhashchange" in window) {
-                console.log("onhashchange is supported")
+                console.log("onhashchange is supported");
             } else {
-                app.get.one(".error").classList.add("show-error")
-                app.get.one(".error").innerHTML = "The browser isn't supporting this app :("
+                app.get.one(".error").classList.add("show-error");
+                app.get.one(".error").innerHTML = "The browser isn't supporting this app :(";
             }
         },
         online: function () {
             if (navigator.onLine) {
-                console.log("you are online")
+                return true;
             } else {
-                app.get.one(".error").classList.add("show-error")
-                app.get.one(".error").innerHTML = "The browser is offline :("
+                app.get.one(".error").classList.add("show-error");
+                app.get.one(".error").innerHTML = "The browser is offline :(";
+                return false;
             }
         }
     }
