f799598bab218f96088784bc9c67389e7171b874
Matth96 <Matthias.Dolstra@hva.nl>
2016-02-17 21:55:26 +0100

    eddit gulp file

--- web-app-from-scratch/opdracht-7-modules/gulpfile.js
+++ web-app-from-scratch/opdracht-7-modules/gulpfile.js
@@ -1,17 +1,31 @@
-const gulp = require('gulp');
-const babel = require('gulp-babel');
-const concat = require('gulp-concat');
+// Load plugins
+var gulp = require('gulp'),
+    babel = require('gulp-babel'),
+    uglify = require('gulp-uglify'),
+    concat = require('gulp-concat'),
+    notify = require('gulp-notify'),
+    livereload = require('gulp-livereload'),
+    del = require('del');
 
-gulp.task('default', ['combineJS', 'worker', 'combineCSS']);
+// Styles
+gulp.task('styles', function (cb) {
+    gulp.src(['./src/css/reset.css', './src/css/loading.css', './src/css/main.css', './src/css/nav.css', './src/css/citys.css', './src/css/search.css'])
+        .pipe(concat('main.css'))
+        .pipe(gulp.dest('dist/css/'))
+});
 
-// takes in a callback so the engine knows when it'll be done
-gulp.task('combineJS', function (cb) {
+// Scripts
+gulp.task('scripts', function (cb) {
     gulp.src(['./src/js/weatherAppStart.js', './src/js/weatherAppLauncher.js', './src/js/weatherAppData.js', './src/js/weatherAppPage.js', './src/js/weatherAppTemplate.js', './src/js/weatherAppUx.js', './src/js/weatherAppRoutes.js', './src/js/weatherAppSupport.js', './src/js/weatherAppEnd.js'])
         .pipe(concat('all.js'))
         .pipe(babel({
             presets: ['es2015']
         }))
+        .pipe(uglify())
         .pipe(gulp.dest('dist/js/'))
+        .pipe(notify({
+            message: 'Scripts task complete'
+        }));
 });
 
 gulp.task('worker', () =>
@@ -19,11 +33,25 @@ gulp.task('worker', () =>
     .pipe(babel({
         presets: ['es2015']
     }))
+    .pipe(uglify())
     .pipe(gulp.dest('dist/js/'))
 );
 
-gulp.task('combineCSS', function (cb) {
-    gulp.src(['./src/css/reset.css', './src/css/loading.css', './src/css/main.css', './src/css/nav.css', './src/css/citys.css', './src/css/search.css'])
-        .pipe(concat('main.css'))
-        .pipe(gulp.dest('dist/css/'))
+
+
+// Default task
+gulp.task('default', function () {
+    gulp.start('styles', 'scripts', 'worker', 'watch');
+});
+
+// Watch
+gulp.task('watch', function () {
+    // Watch .scss files
+    gulp.watch('src/css/*.css', ['styles']);
+    // Watch .js files
+    gulp.watch('src/js/*.js', ['scripts']);
+    // Create LiveReload server
+    livereload.listen();
+    // Watch any files in dist/, reload on change
+    gulp.watch(['dist/**']).on('change', livereload.changed);
 });
